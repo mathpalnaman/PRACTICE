b11c4a254f8c043d88aa1f37c8045893ae4451a0
mathpalnaman <112200396+mathpalnaman@users.noreply.github.com>
2023-01-05 23:40:31 +0530

    Create gfg recursive binary search.cpp

---
+++ gfg recursive binary search.cpp
@@ -0,0 +1,8 @@
+   binarySearch(arr, x, low, high)
+           if low > high
+               return False 
+   
+           else
+               mid = (low + high) / 2 
+                   if x == arr[mid]
+                   return mid
